1d4da9ca5021937c8fb6f76ed30ba9c1e7797185
Yann Diorcet <yann.diorcet@xenocs.com>
2018-07-11 12:10:25 +0200

    Add AsyncCircusConsumer

--- circus/consumer.py
+++ circus/consumer.py
@@ -1,9 +1,46 @@
 import errno
 import zmq
+from zmq.eventloop.zmqstream import ZMQStream
+import tornado
 
 from circus.util import DEFAULT_ENDPOINT_SUB, get_connection
 from circus.py3compat import b
 
+class AsyncCircusConsumer(object):
+    def __init__(self, topics, callback, context=None, endpoint=DEFAULT_ENDPOINT_SUB,
+                 ssh_server=None):
+        self.topics = topics
+        self.keep_context = context is not None
+        self._init_context(context)
+        self.endpoint = endpoint
+        self.pubsub_socket = self.context.socket(zmq.SUB)
+        get_connection(self.pubsub_socket, self.endpoint, ssh_server)
+        for topic in self.topics:
+            self.pubsub_socket.setsockopt(zmq.SUBSCRIBE, b(topic))
+        self.stream = ZMQStream(self.pubsub_socket, tornado.ioloop.IOLoop.instance())
+        def inner_callback(a):
+            callback(a[0], a[1])
+        self.stream.on_recv(inner_callback)
+
+    def _init_context(self, context):
+        self.context = context or zmq.Context()
+
+    def stop(self):
+        self.stream.stop_on_recv()
+        # only supported by libzmq >= 3
+        if hasattr(self.pubsub_socket, 'disconnect'):
+            self.pubsub_socket.disconnect(self.endpoint)
+        self.stream.close()
+
+        if self.keep_context:
+            return
+        try:
+            self.context.destroy(0)
+        except zmq.ZMQError as e:
+            if e.errno == errno.EINTR:
+                pass
+            else:
+                raise
 
 class CircusConsumer(object):
     def __init__(self, topics, context=None, endpoint=DEFAULT_ENDPOINT_SUB,
